6c9a8b8a07ac7eac59fb36dee136969b0936bfdc
soyoungcareer <soyoungcareer@gmail.com>
2021-12-15 17:17:48 +0900

    1215 프로젝트 생성

---
+++ src/main/java/com/mupin/Mupin/MupinApplication.java
@@ -0,0 +1,13 @@
+package com.mupin.Mupin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MupinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MupinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mupin/Mupin/MupinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mupin.Mupin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MupinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
